a0ffc067ca7316bd608a720431a2ddffb9da4b25
tknzk <tkm.knzk@gmail.com>
2017-12-20 16:26:32 +0900

    add json response

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'bundler/setup'
 require 'sinatra'
+require 'sinatra/json'
 
 require 'haml'
 require 'sass'
@@ -10,4 +11,14 @@ get '/' do
   @my_numbers = 10.times.map { Tsubaki::MyNumber.rand }
   @corp_numbers = 10.times.map { Tsubaki::CorporateNumber.rand }
   haml :index
+end
+
+get '/my_numbers.json' do
+  my_numbers = 10.times.map { Tsubaki::MyNumber.rand }
+  json my_numbers
+end
+
+get '/corp_numbers.json' do
+  corp_numbers = 10.times.map { Tsubaki::CorporateNumber.rand }
+  json corp_numbers
 end
